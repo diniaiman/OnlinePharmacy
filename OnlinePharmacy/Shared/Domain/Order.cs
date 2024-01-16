71e026527c3dc6205b51b176bd87c84f6da4c1a1
NOR ISZAFI BIN ISMAIL 2204126C <2204126C@student.tp.edu.sg>
2024-01-16 13:56:13 +0800

    Order & Product Entities

---
+++ OnlinePharmacy/Shared/Domain/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlinePharmacy.Shared.Domain
+{
+    public class Order : BaseDomainModel
+    {
+        public string? Order_Date { get; set; }
+        public string? Order_Status { get; set; }
+        public virtual User? User { get; set; }
+        public virtual Staff? Staff { get; set; }
+
+     
+    }
+}
---
+++ OnlinePharmacy/Shared/Domain/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlinePharmacy.Shared.Domain
+{
+    public class Product : BaseDomainModel
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double? Price { get; set; }
+        public int? Quantity { get; set; }
+        public string? Category { get; set; }
+    }
+}
