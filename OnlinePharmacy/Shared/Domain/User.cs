a72c438d71f771346d39dfd2febfbedcb90aac6a
NOR ISZAFI BIN ISMAIL 2204126C <2204126C@student.tp.edu.sg>
2024-01-16 13:37:22 +0800

    annyeonghaseyo

--- OnlinePharmacy/Shared/Domain/User.cs
+++ OnlinePharmacy/Shared/Domain/User.cs
@@ -8,6 +8,13 @@ namespace OnlinePharmacy.Shared.Domain
 {
     public class User : BaseDomainModel
     {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNo { get; set; }
+        public string? Gender { get; set; }
+        public string? DateOfBirth { get; set; }
 
     }
 }
