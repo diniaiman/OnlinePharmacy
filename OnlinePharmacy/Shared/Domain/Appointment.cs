709aa77eb2bc786850eabe71999a2102aecac2b6
DINI AIMAN BIN ROSLAN 2202035H <2202035H@student.tp.edu.sg>
2024-01-16 14:02:06 +0800

    Appointment & Prescription Entities

---
+++ OnlinePharmacy/Shared/Domain/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlinePharmacy.Shared.Domain
+{
+    public class Appointment : BaseDomainModel
+    {
+        public string? Appointment_Date { get; set; }
+        public string? Appointment_Time { get; set; }
+        public string? Appointment_Status { get; set; }
+        public virtual User? User { get; set; }
+        public virtual Staff? Staff { get; set; }
+    }
+}
---
+++ OnlinePharmacy/Shared/Domain/Prescription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlinePharmacy.Shared.Domain
+{
+    public class Prescription : BaseDomainModel
+    {
+        public string? Prescription_Date { get; set; }
+        public string? Prescription_Details { get; set; }
+        public virtual User? User { get; set; }
+        public virtual Staff? Staff { get; set; }
+    }
+}
