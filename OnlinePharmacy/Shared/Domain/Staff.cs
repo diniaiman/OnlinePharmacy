e8bca821e528d127f10b10e30358bb66a86364c0
DINI AIMAN BIN ROSLAN 2202035H <2202035H@student.tp.edu.sg>
2024-01-16 13:44:47 +0800

    staff entity

---
+++ OnlinePharmacy/Shared/Domain/Staff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlinePharmacy.Shared.Domain
+{
+    public class Staff : BaseDomainModel
+    {
+        public string? StaffType { get; set; }
+        public string? Name { get; set; }
+        public string? Gender { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNo { get; set; }
+        public string? Permissions { get; set; }
+        public string? DateOfBirth { get; set; }
+    }
+}
