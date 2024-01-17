430f0d1deca05b7f41d57bf2b1cabb025a1ee143
DINI AIMAN BIN ROSLAN 2202035H <2202035H@student.tp.edu.sg>
2024-01-17 09:27:12 +0800

    Seed Configuration

---
+++ OnlinePharmacy/Server/Configurations/Entities/ProductSeedConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlinePharmacy.Shared.Domain;
+
+namespace OnlinePharmacy.Server.Configurations.Entities
+{
+    public class ProductSeedConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "CeraVe Cleanser",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Cetaphil Moisturizer",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                }
+                );
+        }
+    }
+}
--- OnlinePharmacy/Server/Data/ApplicationDbContext.cs
+++ OnlinePharmacy/Server/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using OnlinePharmacy.Server.Configurations.Entities;
 using OnlinePharmacy.Server.Models;
 using OnlinePharmacy.Shared.Domain;
 
@@ -21,5 +22,15 @@ namespace OnlinePharmacy.Server.Data
         public DbSet<Order> Orders { get; set; }
         public DbSet<Prescription> Prescriptions { get; set; }
         public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new ProductSeedConfiguration());
+            builder.ApplyConfiguration(new RoleSeedConfiguration());
+            builder.ApplyConfiguration(new UserSeedConfiguration());
+            builder.ApplyConfiguration(new UserRoleSeedConfiguration());
+        }
     }
 }
