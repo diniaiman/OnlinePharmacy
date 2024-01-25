a579b3302c84fbb1db79aaffb0fb2f4c2ba6a728
DINI AIMAN BIN ROSLAN 2202035H <2202035H@student.tp.edu.sg>
2024-01-25 14:01:07 +0800

    Prescription Create

--- OnlinePharmacy/Shared/Domain/Prescription.cs
+++ OnlinePharmacy/Shared/Domain/Prescription.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,28 @@ namespace OnlinePharmacy.Shared.Domain
 {
     public class Prescription : BaseDomainModel
     {
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOut { get; set; }
+        public DateTime? DateIn { get; set; }
         public string? Prescription_Date { get; set; }
         public string? Prescription_Details { get; set; }
         public virtual User? User { get; set; }
+        public int? UserId { get; set; }
         public virtual Staff? Staff { get; set; }
+        public int StaffId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            //throw new NotImplementedException();
+
+            if (DateIn != null)
+            {
+                if (DateIn <= DateOut)
+                {
+                    yield return new ValidationResult("DateIn must be greater than DateOut", new[] { "DateIn" });
+                }
+            }
+        }
     }
 }
