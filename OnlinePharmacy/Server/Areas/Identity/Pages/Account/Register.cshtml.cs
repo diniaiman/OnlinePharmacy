cba8d26ec7fb49cd344be093a53a59e4f4b66dd7
DINI AIMAN BIN ROSLAN 2202035H <2202035H@student.tp.edu.sg>
2024-02-03 19:20:08 +0800

    Login and Register Page Fixed, basic js.

--- OnlinePharmacy/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ OnlinePharmacy/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -30,13 +30,15 @@ namespace OnlinePharmacy.Server.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<ApplicationUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public RegisterModel(
             UserManager<ApplicationUser> userManager,
             IUserStore<ApplicationUser> userStore,
             SignInManager<ApplicationUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -44,6 +46,7 @@ namespace OnlinePharmacy.Server.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _roleManager = roleManager;
         }
 
         /// <summary>
@@ -75,6 +78,14 @@ namespace OnlinePharmacy.Server.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
@@ -115,12 +126,23 @@ namespace OnlinePharmacy.Server.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
+                //Set user first and last name
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
+                    //Set user role to user
+                    if (!await _roleManager.RoleExistsAsync("User"))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole("User"));
+                    }
+                    await _userManager.AddToRoleAsync(user, "User");
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
--- OnlinePharmacy/Server/Migrations/20240203110827_newdb.cs
+++ OnlinePharmacy/Server/Migrations/20240203110827_newdb.cs
@@ -456,15 +456,15 @@ namespace OnlinePharmacy.Server.Migrations
             migrationBuilder.InsertData(
                 table: "AspNetUsers",
                 columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
-                values: new object[] { "3781efa7-66dc-47f0-860f-e506d04102e4", 0, "14bad4ac-f2f7-4b50-b081-96af42834b19", "admin@localhost.com", false, "Admin", "User", false, null, "ADMIN@LOCALHOST.COM", "ADMIN@LOCALHOST.COM", "AQAAAAIAAYagAAAAEDxxxXB0sbca1/UBSlhlCAQ6Qc4g3G7+loelfimObRra3dcR+74MldHmIQCHGtCK7g==", null, false, "ece8d590-e341-4c14-af75-8af52d8ed32a", false, "admin@localhost.com" });
+                values: new object[] { "3781efa7-66dc-47f0-860f-e506d04102e4", 0, "bd0cdcf1-8a99-4a43-86d2-48b4bc5b379e", "admin@localhost.com", false, "Admin", "User", false, null, "ADMIN@LOCALHOST.COM", "ADMIN@LOCALHOST.COM", "AQAAAAIAAYagAAAAEHlqmcBv8qqR0xpunhslZAH3LS/Y+qGzYDbNA+eFenBPNS1R0pfXh6ZO7F4/uqk8Cw==", null, false, "e67b6e37-3ac5-4f67-ba18-0684caebc899", false, "admin@localhost.com" });
 
             migrationBuilder.InsertData(
                 table: "Products",
                 columns: new[] { "Id", "Category", "CreatedBy", "DateCreated", "DateUpdated", "Description", "Name", "Price", "Quantity", "UpdatedBy" },
                 values: new object[,]
                 {
-                    { 1, "Cleansers", "System", new DateTime(2024, 1, 31, 12, 28, 7, 741, DateTimeKind.Local).AddTicks(4733), new DateTime(2024, 1, 31, 12, 28, 7, 741, DateTimeKind.Local).AddTicks(4754), "Description", "CeraVe Cleanser", 20.0, 9, "System" },
-                    { 2, "Moisturizers", "System", new DateTime(2024, 1, 31, 12, 28, 7, 741, DateTimeKind.Local).AddTicks(4761), new DateTime(2024, 1, 31, 12, 28, 7, 741, DateTimeKind.Local).AddTicks(4763), "Description", "Cetaphil Moisturizer", 20.0, 8, "System" }
+                    { 1, "Cleansers", "System", new DateTime(2024, 2, 3, 19, 8, 27, 708, DateTimeKind.Local).AddTicks(782), new DateTime(2024, 2, 3, 19, 8, 27, 708, DateTimeKind.Local).AddTicks(794), "Description", "CeraVe Cleanser", 20.0, 9, "System" },
+                    { 2, "Moisturizers", "System", new DateTime(2024, 2, 3, 19, 8, 27, 708, DateTimeKind.Local).AddTicks(796), new DateTime(2024, 2, 3, 19, 8, 27, 708, DateTimeKind.Local).AddTicks(797), "Description", "Cetaphil Moisturizer", 20.0, 8, "System" }
                 });
 
             migrationBuilder.InsertData(
